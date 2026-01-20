6e1d5d158b41afe6959f14c65eb9fb20b1ff2305
Yuki TAKAGI <141538661+yuki-takagi-66@users.noreply.github.com>
2026-01-20 20:17:54 +0900

    refactor: apply auto-formatting (#4)
    Signed-off-by: yuki-takagi-66 <yuki.takagi@tier4.jp>

--- src/map_area_filter_exec.cpp
+++ src/map_area_filter_exec.cpp
@@ -1,11 +1,12 @@
 #include "map_area_filter_contents.hpp"
-#include"rclcpp/rclcpp.hpp"
+#include "rclcpp/rclcpp.hpp"
 
-int main(int argc, char **argv){
-    rclcpp::init(argc,argv); ////ROS2通信を初期化
-    rclcpp::NodeOptions node_options;
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);  ////ROS2通信を初期化
+  rclcpp::NodeOptions node_options;
 
-    rclcpp::spin(std::make_shared<map_area_filter::MapAreaFilterComponent>(node_options));
-    
-    return 0;
+  rclcpp::spin(std::make_shared<map_area_filter::MapAreaFilterComponent>(node_options));
+
+  return 0;
 }
